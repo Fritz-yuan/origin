6dc8b37bbc90921568ce76bae3ef9af4c1f528e1
Fuyuan Zhang <ucqlf@student.kit.edu>
2023-06-05 12:09:06 +0200

    launchfile adjust

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/yuan/rosyolov5_ws/src
+# Build directory: /home/yuan/rosyolov5_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("detection_msgs")
+subdirs("yolov5_ros")
---
+++ build/detection_msgs/catkin_generated/detection_msgs-msg-extras.cmake.develspace.in
@@ -0,0 +1,2 @@
+set(detection_msgs_MESSAGE_FILES "/home/yuan/rosyolov5_ws/src/detection_msgs/msg/BoundingBox.msg;/home/yuan/rosyolov5_ws/src/detection_msgs/msg/BoundingBoxes.msg")
+set(detection_msgs_SERVICE_FILES "")
