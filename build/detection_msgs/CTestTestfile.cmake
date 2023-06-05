2d2664040e187ed87ab9caa1966324ff11c285ba
Fuyuan Zhang <ucqlf@student.kit.edu>
2023-06-05 12:12:07 +0200

    Revert "launchfile adjust"
    This reverts commit 6dc8b37bbc90921568ce76bae3ef9af4c1f528e1.

--- build/detection_msgs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yuan/rosyolov5_ws/src/detection_msgs
-# Build directory: /home/yuan/rosyolov5_ws/build/detection_msgs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/detection_msgs/catkin_generated/detection_msgs-msg-extras.cmake.develspace.in
+++
@@ -1,2 +0,0 @@
-set(detection_msgs_MESSAGE_FILES "/home/yuan/rosyolov5_ws/src/detection_msgs/msg/BoundingBox.msg;/home/yuan/rosyolov5_ws/src/detection_msgs/msg/BoundingBoxes.msg")
-set(detection_msgs_SERVICE_FILES "")
